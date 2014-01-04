afcefacd77a5df36ee21eaa9eaef18d1c86c978a
Memming Park <memming@utexas.edu>
2014-01-03 23:16:01 -0600

    Many-to-many Tag object added

--- admin.py
+++ admin.py
@@ -1,6 +1,7 @@
 from django.contrib import admin
 
-from ownYourLife.models import Category, Entry
+from ownYourLife.models import Category, Entry, Tag
 
 admin.site.register(Entry)
 admin.site.register(Category)
+admin.site.register(Tag)
--- models.py
+++ models.py
@@ -1,9 +1,18 @@
 from django.db import models
 
+class Tag(models.Model):
+    """ Tags for each Category or additionally for each Entry """
+    name = models.CharField(max_length=63)
+    details = models.TextField()
+
+    def __str__(self):
+        return self.name
+
 class Category(models.Model):
     """ Hierarchical categories """
     name = models.CharField(max_length=255)
     details = models.TextField()
+    tags = models.ManyToManyField(Tag)
 
     def __str__(self):
         return self.name
@@ -12,8 +21,9 @@ class Entry(models.Model):
     """ Each time stamped log entry """
     timestamp = models.DateTimeField('date published')
     cat = models.ForeignKey(Category)
-    value = models.FloatField(default=float('NaN'))
+    value = models.FloatField(default=0)
     note = models.TextField()
+    tags = models.ManyToManyField(Tag)
 
     def __str__(self):
         return '[' + str(self.cat) + '] ' + str(self.value) + ' (' + str(self.timestamp) + ')'

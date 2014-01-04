0a545a148cc6f2f898cf0900bdd50ab5af56e661
Memming Park <memming@utexas.edu>
2014-01-03 19:54:30 -0600

    Basic data structures "Entry" and "Category"

--- README.md
+++ README.md
@@ -3,4 +3,15 @@ ownYourLife
 
 Life log tool for events, measurements, and time series
 
-Python3/Django
+Python3/Django web application
+
+Model
+=====
+
+ * Entry: a time stamped generic entry
+    * cat: key to category 
+    * value: a number or python string summarizing the event
+    * note: extra string for non-structured note
+
+ * Category: string representing a hierarchical entry type
+    * hierarchy promotes quick selection of entry type
--- models.py
+++ models.py
@@ -1,3 +1,13 @@
 from django.db import models
 
-# Create your models here.
+class Category(models.Model):
+    """ Hierarchical categories """
+    name = models.CharField(max_length=255)
+    details = models.TextField()
+
+class Entry(models.Model):
+    """ Each time stamped log entry """
+    timestamp = models.DateTimeField('date published')
+    cat = models.ForeignKey(Category)
+    value = models.FloatField(default=NaN)
+    note = models.TextField()
